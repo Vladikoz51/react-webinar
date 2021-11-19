32a47e0e7ff2c9afbd73636cd45fd36b0479a8d9
vladikoz51 <vladikoz51@gmail.com>
2021-11-19 17:13:45 +0500

    Созданы компоненты ListItem и Item для решения задания №1.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.css';
+import {ListItem} from "./listItem";
 
 /**
  * Приложение
  * @param store {Store} Состояние с действиями
  */
+
 function App({store}) {
   return (
     <div className='App'>
@@ -16,20 +18,21 @@ function App({store}) {
       </div>
       <div className='App__center'>
         <div className='List'>{store.getState().items.map(item =>
-          <div
-            key={item.code}
-            className={'List__item' + (item.selected ? ' List__item_selected' : '')}
-          >
-            <div className='Item' onClick={() => store.selectItem(item.code)}>
-              <div className='Item__number'>{item.code}</div>
-              <div className='Item__title'>{item.title}</div>
-              <div className='Item__actions'>
-                <button onClick={() => store.deleteItem(item.code)}>
-                  Удалить
-                </button>
-              </div>
-            </div>
-          </div>
+          <ListItem store={store} item={item} />
+          // <div
+          //   key={item.code}
+          //   className={'List__item' + (item.selected ? ' List__item_selected' : '')}
+          // >
+          //   <div className='Item' onClick={() => store.selectItem(item.code)}>
+          //     <div className='Item__number'>{item.code}</div>
+          //     <div className='Item__title'>{item.title}</div>
+          //     <div className='Item__actions'>
+          //       <button onClick={() => store.deleteItem(item.code)}>
+          //         Удалить
+          //       </button>
+          //     </div>
+          //   </div>
+          // </div>
         )}
         </div>
       </div>
---
+++ src/listItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Item} from "./item";
+
+export class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false
+    }
+  }
+
+  render() {
+    return (
+      <div
+        key={this.props.item.code}
+        className={'List__item' + (this.state.selected ? ' List__item_selected' : '')}
+        onClick={() => {this.setState({selected: !this.state.selected});}}
+      >
+        <Item store={this.props.store} item={this.props.item} />
+      </div>
+    );
+  }
+}
